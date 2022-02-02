c08ecc395f3c55b693113d33706311985e1d23da
Daniel Martinez <danielmartinez@nagarro.com>
2022-02-02 13:49:22 -0600

    first commit

---
+++ billing/src/main/java/com/paymentchain/billing/entities/Invoice.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor. Nothing has really changed
+ */
+package com.paymentchain.billing.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Data;
+
+
+
+/**
+ *
+ * @author sotobotero
+ */
+@Entity
+@Data
+public class Invoice {
+   @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+   private long id;
+   private long customerId;
+   private String number;
+   private String detail;
+   private double amount;
+   public void setId(int i) {
+   }
+public Long getId() {
+    return null;
+}  
+}
